4543f328160b11be10d3c8970f2538f5ba29d435
Mike Roberts <miker@miker.org>
2015-04-13 14:26:51 -0400

    javacv works!

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,9 @@ object Seurat extends Build {
         "-source","1.8",
         "-target","1.8"
       ),
-      promptTheme := prompt
+      promptTheme := prompt,
+      libraryDependencies ++= deps,
+      classpathTypes += "maven-plugin"
     )
   )
 
@@ -35,4 +37,50 @@ object Seurat extends Build {
     text(")", fg(green)),
     text("> ", fg(green))
   ))
+
+  val javacppVersion = "0.10"
+
+  // Determine current platform
+  val platform = {
+    // Determine platform name using code similar to javacpp
+    // com.googlecode.javacpp.Loader.java line 60-84
+    val jvmName = System.getProperty("java.vm.name").toLowerCase
+    var osName = System.getProperty("os.name").toLowerCase
+    var osArch = System.getProperty("os.arch").toLowerCase
+    if (jvmName.startsWith("dalvik") && osName.startsWith("linux")) {
+      osName = "android"
+    } else if (jvmName.startsWith("robovm") && osName.startsWith("darwin")) {
+      osName = "ios"
+      osArch = "arm"
+    } else if (osName.startsWith("mac os x")) {
+      osName = "macosx"
+    } else {
+      val spaceIndex = osName.indexOf(' ')
+      if (spaceIndex > 0) {
+        osName = osName.substring(0, spaceIndex)
+      }
+    }
+    if (osArch.equals("i386") || osArch.equals("i486") || osArch.equals("i586") || osArch.equals("i686")) {
+      osArch = "x86"
+    } else if (osArch.equals("amd64") || osArch.equals("x86-64") || osArch.equals("x64")) {
+      osArch = "x86_64"
+    } else if (osArch.startsWith("arm")) {
+      osArch = "arm"
+    }
+    val platformName = osName + "-" + osArch
+    println("platform: " + platformName)
+    platformName
+  }
+
+  val deps = Seq(
+    "org.imgscalr" % "imgscalr-lib" % "4.2" % "compile",
+    "ch.qos.logback" % "logback-classic" % "1.1.2" % "compile",
+    "org.bytedeco" % "javacpp" % javacppVersion,
+    "org.bytedeco" % "javacv" % javacppVersion,
+    "org.bytedeco.javacpp-presets" % "opencv" % ("2.4.10-" + javacppVersion) classifier "",
+    "org.bytedeco.javacpp-presets" % "opencv" % ("2.4.10-" + javacppVersion) classifier platform,
+    "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.11.6" % "test"
+  )
+
 }
--- src/main/scala/org/miker/seurat/Main.scala
+++ src/main/scala/org/miker/seurat/Main.scala
@@ -1,1 +1,59 @@
-object Hi { def main(args: Array[String]) = println("Hi!") }
+import java.awt.image.BufferedImage
+import java.io.{IOException, File}
+import javax.imageio.ImageIO
+import org.bytedeco.javacpp.opencv_core.cvLoad
+import org.bytedeco.javacpp.opencv_core.IplImage
+import org.bytedeco.javacpp.opencv_objdetect.CvHaarClassifierCascade
+import org.bytedeco.javacv.CanvasFrame
+import org.slf4j.LoggerFactory
+import javax.swing.JFrame._
+import org.bytedeco.javacpp.opencv_highgui._
+
+
+object Main {
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  //val defaultCascade = new CvHaarClassifierCascade(cvLoad(CASCADE_PATH + "haarcascade_frontalface_alt.xml"));
+  //val profileCascade = new CvHaarClassifierCascade(cvLoad(CASCADE_PATH + "haarcascade_profileface.xml"));
+
+
+  def main(args: Array[String]) = {
+    // Read an image
+    val image = imread("data/IMG_0350.jpg")
+    if (image.empty()) {
+      // error handling
+      // no image has been created...
+      // possibly display an error message
+      // and quit the application
+      println("Error reading image...")
+      System.exit(0)
+    }
+
+    // Create image window named "My Image".
+    //
+    // Note that you need to indicate to CanvasFrame not to apply gamma correction,
+    // by setting gamma to 1, otherwise the image will not look correct.
+    val canvas = new CanvasFrame("My Image", 1)
+
+    // Request closing of the application when the image window is closed
+    canvas.setDefaultCloseOperation(EXIT_ON_CLOSE)
+
+    // Show image on window
+    canvas.showImage(image)
+  }
+
+  def load(filename: String): Option[BufferedImage] = {
+    try {
+      Some(ImageIO.read(new File(filename)))
+    } catch {
+      case e: IOException => {
+        logger.error(s"error loading file ${filename}")
+        None
+      }
+    }
+  }
+
+  def detectFaces(i: IplImage) = {
+
+  }
+}
